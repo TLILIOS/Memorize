a7068026cbf56bb7c902549c2e25a341662a9bf8
TLiLi HAMDI <tlilios@yahoo.com>
2024-07-02 03:06:34 +0200

    MVVM

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,67 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis = ["🦜", "🐬", "🐣","🐸", "🐓", "🦩", "🐇", "🐈", "🦘", "🐄", "🐑", "🐠","🐙","🦀" ].shuffled()
-    @State var cartCount = 4
     var body: some View {
-        VStack {
             ScrollView {
                 cards
             }
-            Spacer()
-            cardCountAdjusters
-        }
         .padding()
     }
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-             cartCount += offset
-        }
-               , label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cartCount + offset < 1 || cartCount + offset > emojis.count)
-    }
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
+   
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cartCount, id: \.self) { index in CardView(content: emojis[index])
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 115))]) {
+            ForEach(emojis.indices, id: \.self) { index in CardView(content: emojis[index])
                     .aspectRatio(2/3,contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
     }
     
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
-//        Button(action: {
-//            if cartCount > 1 {
-//                cartCount -= 1
-//            }
-//        }
-//               , label: {
-//            Image(systemName:"rectangle.stack.fill.badge.minus")
-//        })
-    }
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
-//        Button(action: {
-//            if  cartCount < emojis.count {
-//                cartCount += 1
-//            }
-//        },
-//               label: {
-//            Image(systemName: "rectangle.stack.fill.badge.plus")
-//        })
-    }
 }
 
 
@@ -92,7 +48,6 @@ struct CardView: View {
             base.fill().opacity(isFaceUp ? 0: 1)
         }
         .onTapGesture {
-            print("Flip Over")
             isFaceUp.toggle()
         }
     }
---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by HTLILI on 02/07/2024.
+//
+
+import Foundation
